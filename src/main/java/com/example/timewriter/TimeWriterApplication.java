0c313bc04978960ae1c5fd10ee4771b754d9d541
Vlad <vlad@example.com>
2025-06-07 21:11:37 +0500

    Initial commit

---
+++ src/main/java/com/example/timewriter/TimeWriterApplication.java
@@ -0,0 +1,13 @@
+package com.example.timewriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeWriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/timewriter/TimeWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timewriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
