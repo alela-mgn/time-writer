c71e9daca311ba5def929ab125c4d749284d5d2d
Vlad <vlad@example.com>
2025-06-08 00:24:22 +0500

    TW-1 | добавлена миграция бд

--- src/main/java/com/leon/timewriter/TimeWriterApplication.java
+++ src/main/java/com/leon/timewriter/TimeWriterApplication.java
@@ -1,4 +1,4 @@
-package com.example.timewriter;
+package com.leon.timewriter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/leon/timewriter/TimeWriterApplicationTests.java
+++ src/test/java/com/leon/timewriter/TimeWriterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.timewriter;
+package com.leon.timewriter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
